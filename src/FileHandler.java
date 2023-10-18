56d4edf13995c1669f04f60fc637c31d7f4ecdb3
Sylwia Jaworska <se.jaworska@gmail.com>
2023-10-19 00:26:20 +0200

    try catch add

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -1,4 +1,5 @@
 import java.io.*;
+
 public class FileHandler {
     public static void addToFile(Object object, File fileName) throws IOException {
         FileWriter f = new FileWriter(fileName, true);
@@ -11,13 +12,18 @@ public class FileHandler {
         }
     }
 
-    public static void readFromFile(File fileName) throws IOException {
-        FileInputStream f = new FileInputStream(fileName);
-        DataInputStream in = new DataInputStream(f);
-        BufferedReader r = new BufferedReader(new InputStreamReader(in));
-        String strLine;
-        while ((strLine = r.readLine()) != null)
-            System.out.println(strLine);
-        in.close();
+    public static void readFromFile(File fileName) {
+        try {
+            FileInputStream f = new FileInputStream(fileName);
+            DataInputStream in = new DataInputStream(f);
+            BufferedReader r = new BufferedReader(new InputStreamReader(in));
+            String strLine;
+            int i = 1;
+            while ((strLine = r.readLine()) != null)
+                System.out.println(i++ + ". " + strLine);
+            in.close();
+        } catch (IOException e) {
+            System.out.println("Błąd dostępu do pliku");
+        }
     }
 }
