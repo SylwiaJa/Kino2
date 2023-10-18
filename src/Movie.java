e415c2a2bc1bc45e14d6e4bcc367add1a81948ef
Sylwia Jaworska <se.jaworska@gmail.com>
2023-10-19 00:20:49 +0200

    renaming, removing unused methods

--- src/Movie.java
+++ src/Movie.java
@@ -5,86 +5,36 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Movie implements Serializable {
-    private  String tytul;
-    private LocalDate dzien;
-    private LocalTime godzina;
-    private  Integer ograniczeniaWiekowe;
-    private HashMap<Character, HashMap<Integer,Boolean>> liczbaMiejsc;
+    private String title;
+    private LocalDate day;
+    private LocalTime hour;
+    private Integer ageRestriction;
+    private HashMap<Character, HashMap<Integer, Boolean>> numberOfSeats;
 
-    public Movie(String tytul, LocalDate dzien, LocalTime godzina, Integer ograniczeniaWiekowe, HashMap<Character, HashMap<Integer, Boolean>> liczbaMiejsc) {
-        this.tytul = tytul;
-        this.dzien = dzien;
-        this.godzina = godzina;
-        this.ograniczeniaWiekowe = ograniczeniaWiekowe;
-        this.liczbaMiejsc = liczbaMiejsc;
-    }
-
-    public String getTytul() {
-        return tytul;
-    }
-
-    public void setTytul(String tytul) {
-        this.tytul = tytul;
-    }
-
-    public LocalDate getDzien() {
-        return dzien;
-    }
-
-    public void setDzien(LocalDate dzien) {
-        this.dzien = dzien;
-    }
-
-    public LocalTime getGodzina() {
-        return godzina;
-    }
-
-    public void setGodzina(LocalTime godzina) {
-        this.godzina = godzina;
-    }
-
-    public Integer getOgraniczeniaWiekowe() {
-        return ograniczeniaWiekowe;
-    }
-
-    public void setOgraniczeniaWiekowe(Integer ograniczeniaWiekowe) {
-        this.ograniczeniaWiekowe = ograniczeniaWiekowe;
-    }
-
-    public HashMap<Character, HashMap<Integer, Boolean>> getLiczbaMiejsc() {
-        return liczbaMiejsc;
-    }
-
-    public void setLiczbaMiejsc(HashMap<Character, HashMap<Integer, Boolean>> liczbaMiejsc) {
-        this.liczbaMiejsc = liczbaMiejsc;
+    public Movie(String title, LocalDate day, LocalTime hour, Integer ageRestriction, HashMap<Character, HashMap<Integer, Boolean>> numberOfSeats) {
+        this.title = title;
+        this.day = day;
+        this.hour = hour;
+        this.ageRestriction = ageRestriction;
+        this.numberOfSeats = numberOfSeats;
     }
 
     @Override
     public String toString() {
-        return tytul + ";"+
-              dzien +";"+
-                godzina +";"+
-                 ograniczeniaWiekowe +";"+
-                liczbaMiejsc;
+        return "Tytul: " + title + ", Data: " + day + ", Godzina: " + hour + ", Wiek: " + ageRestriction;
 
     }
-    public String toDisplay(){
-        return
-                "Tytul: " + tytul + '\n' +
-                        "Data: " + dzien + '\n'+
-                        "Godzina: " + godzina +'\n'+
-                        "Ograniczenia wiekowe: " + ograniczeniaWiekowe +'\n';
-    }
-    public void displayCinemaRoom(){
-        System.out.println("Tytuł: "+tytul+"   Data: "+dzien+"   Godzina: "+godzina+"   Wiek: "+ograniczeniaWiekowe);
+
+    public void displayCinemaRoom() {
+        System.out.println("Tytuł: " + title + "   Data: " + day + "   Godzina: " + hour + "   Wiek: " + ageRestriction);
         System.out.println("  0 1 2 3 4 5 6 7 8 9");
-        for (Map.Entry<Character,HashMap<Integer,Boolean>> row:liczbaMiejsc.entrySet()) {
-            char keyRow=row.getKey();
+        for (Map.Entry<Character, HashMap<Integer, Boolean>> row : numberOfSeats.entrySet()) {
+            char keyRow = row.getKey();
             System.out.print(keyRow);
-            HashMap<Integer,Boolean> seats=row.getValue();
-            for (Map.Entry<Integer,Boolean> seat:seats.entrySet()) {
-                int keySeat= seat.getKey();
-                boolean valueSeat=seat.getValue();
+            HashMap<Integer, Boolean> seats = row.getValue();
+            for (Map.Entry<Integer, Boolean> seat : seats.entrySet()) {
+                int keySeat = seat.getKey();
+                boolean valueSeat = seat.getValue();
                 if (valueSeat)
                     System.out.print(" *");
                 else
@@ -94,10 +44,10 @@ public class Movie implements Serializable {
         }
 
     }
-  //  public void reservation(char row, int seat) {
-   //     HashMap<Integer, Boolean> rowMap = new HashMap<>(liczbaMiejsc.get(row)); // Tworzenie kopii rowMap
+    //  public void reservation(char row, int seat) {
+    //     HashMap<Integer, Boolean> rowMap = new HashMap<>(liczbaMiejsc.get(row)); // Tworzenie kopii rowMap
     //    rowMap.put(seat, false);
-   //     liczbaMiejsc.put(row, rowMap);
-   // }
+    //     liczbaMiejsc.put(row, rowMap);
+    // }
 
 }
